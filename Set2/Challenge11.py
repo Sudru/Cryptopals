d61307de3ebca476b57dec7c5c041f2dc57783b0
Sudarshan <sudarshandevkota22@gmail.com>
2023-08-05 20:05:39 +0545

    challenge 11

---
+++ Set2/Challenge11.py
@@ -0,0 +1,43 @@
+from Crypto.Cipher import AES
+import random
+import string
+
+def detect_ecb(line):
+    blocks = [line[i:i+16] for i in range(0,len(line),16)]
+    repeating_block = {}
+    for b in blocks:
+        count = blocks.count(b)
+        if count >1:
+            repeating_block[b]=blocks.count(b)       
+    return len(repeating_block)>0
+
+
+
+def generate_key(key_size):
+    key = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(key_size)])
+    return key.encode()
+
+def encryption_oracle(plaintext):
+    random_key = generate_key(16)
+    mode = random.randint(0,1)
+    ciphertext = ""
+    if mode:
+        print("Choosing ECB")
+        cipher = AES.new(random_key,AES.MODE_ECB)
+        ciphertext = cipher.encrypt(plaintext)
+    else:
+        print("Choosing CBC")
+        cipher = AES.new(random_key,AES.MODE_CBC)
+        ciphertext = cipher.encrypt(plaintext)
+    
+    if detect_ecb(ciphertext):
+        print("Detected ECB")
+    else:
+        print("Detected CBC")
+
+def main():
+    plaintext = b"YELLOW SUBMARINE sudarshan 1904 YELLOW SUBMARINE"
+    encryption_oracle(plaintext)
+
+if __name__ == "__main__":
+    main()
